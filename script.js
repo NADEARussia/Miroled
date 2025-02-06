acd43a64cb0d441e073935e34631be2aa1c2a0bc
NADEARussia <nadea.dima@mail.ru>
2025-02-06 23:38:10 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const intro = document.querySelector('.intro');
-    setTimeout(() => {
-        intro.style.opacity = '0';
-        setTimeout(() => {
-            intro.style.display = 'none';
-        }, 1000);
-    }, 3000);
-});
